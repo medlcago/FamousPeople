e5db522b64406ea56370db11e249308a6d095a9b
medlcago <sasha.vol40@mail.ru>
2024-03-03 18:51:46 +0300

    Незначительные изменения

--- users/backends.py
+++ users/backends.py
@@ -1,25 +1,28 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import BaseBackend
 
+User = get_user_model()
+
 
 class EmailBackend(BaseBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
-        user_model = get_user_model()
         try:
-            user = user_model.objects.get(email=username)
+            user = User.objects.get(email=username)
             if user.check_password(password) and self.user_can_authenticate(user):
                 return user
             return
-        except (user_model.DoesNotExist, user_model.MultipleObjectsReturned):
+        except (User.DoesNotExist, User.MultipleObjectsReturned):
             return
 
     def get_user(self, user_id):
-        user_model = get_user_model()
         try:
-            user = user_model.objects.get(pk=user_id)
-        except user_model.DoesNotExist:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
             return
         return user if self.user_can_authenticate(user) else None
 
     def user_can_authenticate(self, user):
         return getattr(user, "is_active", True)
+
+
+
